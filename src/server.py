a9e0fc1d40cc2d44ec89efb0790379915a0fbb06
matias-gonz <maigonzalez@fi.uba.ar>
2022-09-15 21:42:19 -0300

    Add log to server

--- src/server.py
+++ src/server.py
@@ -1,13 +1,18 @@
+import logging as log
 import socket
 
 HOST = "127.0.0.1"
 PORT = 6543
 
+log.basicConfig(level=log.DEBUG)
+log.info(f"Address: {HOST}:{PORT}")
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
+    log.debug(f"Socket binded to {(HOST, PORT)}")
     s.listen()
     conn, addr = s.accept()
     with conn:
-        print(f"Connected by {addr}")
+        log.info(f"Connected by {addr}")
         data = conn.recv(1024)
         print(data.decode())
