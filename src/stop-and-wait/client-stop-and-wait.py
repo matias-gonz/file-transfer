958e49e09cef00aef324733d11adeefd2362a154
Facundo Pareja <facundopareja@gmail.com>
2022-09-18 19:35:46 -0300

    transmision datos del cliente

--- src/stop-and-wait/client-stop-and-wait.py
+++ src/stop-and-wait/client-stop-and-wait.py
@@ -1,7 +1,6 @@
 import logging as log
-import socket
-
 import constant
+from socket import *
 
 HOST = "127.0.0.1"
 PORT = 6543
@@ -9,16 +8,25 @@ PORT = 6543
 log.basicConfig(level=log.DEBUG)
 log.info(f"Server Address: {HOST}:{PORT}")
 
-
 def first_packet():
-    sequence_number = (constant.FIRST_SEQ_NUMBER).to_bytes(4, byteorder="big")
-    operation_type = (constant.UPLOAD).to_bytes(1, byteorder="big")
+    sequence_number = (constant.FIRST_SEQ_NUMBER).to_bytes(4, byteorder='big')
+    operation_type = (constant.UPLOAD).to_bytes(1, byteorder='big')
     filename = "file.txt".encode()
     return sequence_number + operation_type + filename
 
-
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+def upload():
+    client_socket = socket(AF_INET, SOCK_DGRAM)
     log.debug(f"Sending first message to{(HOST, PORT)}")
-    s.sendto(first_packet(), (HOST, PORT))
-    log.debug(f"Message sent to{(HOST, PORT)}")
-    s.close()
+    client_socket.sendto(first_packet(), (HOST, PORT))
+    log.debug(f"First Message sent to{(HOST, PORT)}")
+    file = open (constant.FILEPATH+'prueba.txt',"rb")
+    data = file.read(constant.MAX_PKT_SIZE)
+    while (data):
+        if (client_socket.sendto(data, (HOST, PORT))):
+            log.debug("Sending data packets")
+            data = file.read(constant.MAX_PKT_SIZE)
+    client_socket.close()
+    file.close()
+
+
+upload()
