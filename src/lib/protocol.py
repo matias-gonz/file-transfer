59ad926da004b0da267e66000d6f5dd744c4d05d
Tomas Gruner <47506558+MegaRedHand@users.noreply.github.com>
2022-09-24 13:09:20 -0300

    Modifico el funcionamiento de Sender

--- src/lib/protocol.py
+++ src/lib/protocol.py
@@ -52,42 +52,27 @@ class Sender:
     def __init__(self, file_name):
         log.debug(f"Reading from file: '{file_name}'")
         self.file = open(file_name, "rb")
-        self.base = 1  # oldest in-flight packet
-        self.dup_ack = 2
-        self.timeout_count = -1
+        self.base = 1  # next expected ack
+        self.last_sent = 0
+        self.dup_ack = 0
+        self.timeout_count = 0
         self.buffer = collections.deque(maxlen=constant.WINDOW_SIZE)
-        for i in range(constant.WINDOW_SIZE):
-            self.read_next_chunk()
 
     def respond_to(self, msg):
         ack_n = int.from_bytes(msg[:4], byteorder="big")
-
-        if ack_n == 0:
-            return self.go_back_n()
-
         log.debug(f"Received ACK={ack_n}")
 
-        if ack_n < self.base:
-            return tuple()
-
-        if ack_n == self.base:
+        if self.base < ack_n <= self.last_sent:
+            self.dup_ack = 0
+            self.timeout_count = 0
+            self.base = ack_n
+        elif ack_n == self.base:
             self.dup_ack = (self.dup_ack + 1) % 3
-            return tuple() if self.dup_ack != 0 else self.timeout_response()
+            if self.dup_ack == 0:
+                return self.timeout_response()
 
-        self.dup_ack = 0
-        self.timeout_count = 0
-
-        msgs = (
-            self.compose_msg(seq_num, self.read_next_chunk())
-            for seq_num in range(
-                self.base + constant.WINDOW_SIZE, ack_n + constant.WINDOW_SIZE
-            )
-        )
-
-        self.base = ack_n
-        log.debug(f"Current base={self.base}")
-
-        return msgs
+        log.debug(f"Current base={self.base}, last_sent={self.last_sent}")
+        return self.fill_window()
 
     def timeout_response(self):
         if self.timeout_count == RETRY_NUMBER:
@@ -98,10 +83,17 @@ class Sender:
         return self.go_back_n()
 
     def go_back_n(self):
-        return (
-            self.compose_msg(i, payload)
-            for i, payload in enumerate(self.buffer, self.base)
+        return iter(self.buffer)
+
+    def fill_window(self):
+        msgs = (
+            self.compose_msg(seq_num, self.read_next_chunk())
+            for seq_num in range(
+                self.last_sent + 1, self.base + constant.WINDOW_SIZE
+            )
         )
+        self.last_sent = self.base + len(self.buffer) - 1
+        return msgs
 
     def compose_msg(self, seq_num, payload):
         return seq_num.to_bytes(4, byteorder="big") + payload
