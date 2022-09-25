6666438aa01d23f32f8edcc500c381375b595a03
Tomas Gruner <47506558+MegaRedHand@users.noreply.github.com>
2022-09-25 14:09:52 -0300

    Utilizo la clase Sender en el cliente

--- src/stop-and-wait/client-stop-and-wait.py
+++ src/stop-and-wait/client-stop-and-wait.py
@@ -1,4 +1,5 @@
 import sys
+from os import path
 sys.path.append('.')  # noqa: E402
 from src.parser import parser as p
 import logging as log
@@ -8,70 +9,72 @@ import src.lib.protocol as protocol
 
 HOST = "127.0.0.1"
 PORT = 6543
-SOCKET_TIMEOUT = 0.030  # 30 ms
-MAX_ATTEMPTS = 180
 
 
 log.basicConfig(level=log.DEBUG)
 log.info(f"Server Address: {HOST}:{PORT}")
 
 
-def sendto_and_ack(s, msg):
+def send_and_ack(s, msg):
     s.sendto(msg, (HOST, PORT))
     msg, address = s.recvfrom(4)
-    return protocol.msg_number(msg)
+    return msg
 
 
-def sendto(s, iseqnum, msg):
+def send_request(s, msg):
     attempts = 0
-    iseqnum += 1
     while True:
         try:
-            ack = sendto_and_ack(s, msg)
-            log.debug(f"ack {ack}, iseqnum {iseqnum}")
-            if ack == iseqnum:
-                return iseqnum
+            msg = send_and_ack(s, msg)
+            ack = protocol.msg_number(msg)
+            log.debug(f"ack {ack}, iseqnum {constant.CONN_START_SEQNUM}")
+            if ack == constant.CONN_START_SEQNUM + 1:
+                return msg
 
-        except timeout:
+        except TimeoutError:
             attempts += 1
-            if attempts >= MAX_ATTEMPTS:
+            if attempts >= constant.RETRY_NUMBER:
                 raise
 
 
 def upload():
     parser = p.upload_parser()
     args = parser.parse_args()
-    src = args.src
+    src = path.expanduser(args.src)
     name = args.name
 
     s = socket(AF_INET, SOCK_DGRAM)
-    s.settimeout(SOCKET_TIMEOUT)
+    s.settimeout(constant.RETRY_DELAY)
 
     log.debug(f"Sending first message to {(HOST, PORT)}")
-    iseqnum = constant.CONN_START_SEQNUM
-    msg = protocol.compose_request_msg(constant.UPLOAD, name)
-    iseqnum = sendto(s, iseqnum, msg)
+    request = protocol.compose_request_msg(constant.UPLOAD, name)
+    msg = send_request(s, request)
     log.debug(f"First Message sent to {(HOST, PORT)}")
 
-    sended_data = 0
-    file = open(src, "rb")
-    data = file.read(constant.PAYLOAD_SIZE)
+    sender = protocol.Sender(src)
 
-    data = bytearray(data)
-    while data:
-        log.debug(
-            f"Sending {len(data)} bytes of data, sequence number is {iseqnum}")
-        msg = protocol.compose_msg(iseqnum, data)
-        iseqnum = sendto(s, iseqnum, msg)
-        sended_data += len(data)
-        data = file.read(constant.PAYLOAD_SIZE)
+    while True:
+        try:
+            responses = sender.respond_to(msg)
 
-    log.debug(f"Sending sequence number is {iseqnum} with no data")
-    msg = protocol.compose_msg(iseqnum)
-    s.sendto(msg, (HOST, PORT))
-    log.debug(f"sended_data {sended_data} bytes of data")
-    s.close()
-    file.close()
+            for resp in responses:
+                s.sendto(resp, (HOST, PORT))
+
+            try:
+                address = tuple()
+                while address != (HOST, PORT):
+                    msg, address = s.recvfrom(constant.MAX_PKT_SIZE)
+
+            except TimeoutError:
+                for resp in sender.timeout_response():
+                    s.sendto(resp, (HOST, PORT))
+
+        except TimeoutError:
+            print("Se perdió la conexión con el servidor")
+            sys.exit(1)
+        except StopIteration:
+            break
 
 
-upload()
+if __name__ == '__main__':
+    upload()
