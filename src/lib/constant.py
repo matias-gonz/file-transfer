3f43256b85ae0a8fbad6b21bfdda9a1b7a0c616f
Sebastian D'Alessandro Szymanowski <dalessandrosj@gmail.com>
2022-09-28 04:40:42 -0300

    Cambiar funcionamiento de la ventana de gbn

--- src/lib/constant.py
+++ src/lib/constant.py
@@ -9,7 +9,7 @@ PAYLOAD_SIZE = MAX_PKT_SIZE - HEADER_SIZE
 
 CONNECTION_TIMEOUT = 0.6  # seconds before connection timeout
 SOCKET_TIMEOUT = 0.030  # socket.recvfrom timeout
-RETRY_NUMBER = 10  # number of retries before cutting the faulty connection
+RETRY_NUMBER = 40  # number of retries before cutting the faulty connection
 WINDOW_SIZE = 10  # size of the sending window (set to 1 for stop-and-wait)
 DUP_ACKS_BEFORE_RETRY = 3  # number of duplicate ACKs before resending packets
 
--- src/lib/protocol.py
+++ src/lib/protocol.py
@@ -181,43 +181,30 @@ class Sender:
 
         self.timeout_count += 1
         # go-back-n
-        msgs = [
-            compose_msg(sequence_number(i), data)
-            for i, data in enumerate(self.buffer, self.base)
-        ]
-        log.debug(f"Resending {len(msgs)} packets")
+        msgs = [msg for _, msg in enumerate(self.buffer, self.base)]
         msgs_lens = ", ".join((str(len(m)) for m in msgs))
         log.debug(
-            f"Sent {len(msgs)} messages, "
+            f"Resend {len(msgs)} messages from {self.base}, "
             + (f"of size {msgs_lens}, " if len(msgs_lens) > 0 else "")
             + f"last_sent={self.last_sent}"
         )
         return msgs
 
     def finished(self):
-        return self.base - 1 == self.final_pkt
+        return self.final_pkt is not None and self.base - 1 == self.final_pkt
 
     def _fill_window(self):
-        n_to_send = self.base - 1 - self.last_sent
+        not_acked = self.last_sent - self.base + 1
+        n_to_remove = len(self.buffer) - not_acked
+        n_to_send = constant.WINDOW_SIZE - not_acked
 
-        if self.final_pkt is not None:
-            for _ in range(len(self.buffer) + n_to_send):
-                self._read_next_chunk()
-            return tuple()
+        self._pop_acked(n_to_remove)
+        msgs = self._push_next_msgs(n_to_send, n_to_remove)
 
-        n_to_send += constant.WINDOW_SIZE
-
-        log.debug(f"Advancing window {n_to_send} spaces")
-
-        msgs = []
-        for i in range(self.last_sent + 1, self.last_sent + 1 + n_to_send):
-            d = self._read_next_chunk()
-            msgs.append(compose_msg(sequence_number(i), d))
-            if len(d) == 0:
-                self.final_pkt = self.last_sent + len(msgs)
-                break
-
-        self.last_sent += len(msgs)
+        transfer_ended = len(msgs) == 0 and not_acked == 0
+        if transfer_ended and self.final_pkt is None:
+            msg = self._push_final_message()
+            msgs.append(msg)
 
         msgs_lens = ", ".join((str(len(m)) for m in msgs))
         log.debug(
@@ -227,14 +214,31 @@ class Sender:
         )
         return msgs
 
-    def _read_next_chunk(self):
-        read = self.file.read(constant.PAYLOAD_SIZE)
-        if len(self.buffer) > 0 and len(self.buffer[-1]) == 0:
-            if len(self.buffer) > 1:
-                self.buffer.popleft()
-        else:
-            self.buffer.append(read)
-        return read
+    def _pop_acked(self, n_to_remove):
+        for _ in range(n_to_remove):
+            self.buffer.popleft()
+
+    def _push_next_msgs(self, n_to_send, n_to_remove):
+        msgs = []
+        for i in range(self.last_sent + 1, self.last_sent + 1 + n_to_send):
+            d = self.file.read(constant.PAYLOAD_SIZE)
+
+            if len(d) == 0:
+                break
+
+            msg = compose_msg(sequence_number(i), d)
+            self.buffer.append(msg)
+            msgs.append(msg)
+
+        self.last_sent += len(msgs)
+        return msgs
+
+    def _push_final_message(self):
+        self.last_sent += 1
+        self.final_pkt = self.last_sent
+        msg = compose_msg(sequence_number(self.final_pkt), )
+        self.buffer.append(msg)
+        return msg
 
     def __del__(self):
         try:
@@ -274,9 +278,7 @@ class Receiver:
             f"Received SEQ={seq_num}, "
             f"expected SEQ={sequence_number(self.next)}"
         )
-        if seq_num + 1 < sequence_number(self.next):
-            return tuple()
-        elif seq_num == sequence_number(self.next):
+        if seq_num == sequence_number(self.next):
             self.timeout_count = 0
 
             if len(data) == 0:
